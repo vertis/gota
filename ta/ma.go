31f572851ab1ac94612a391aae7324b148644a21
Luke Chadwick <me@vertis.io>
2015-08-25 07:22:24 +1000

    Working SMA and EMA

--- ta/ma.go
+++ ta/ma.go
@@ -17,12 +17,28 @@ func Sma(values []interface{}, period int) []interface{}{
   for index,_ := range values {
     indexPlusOne := index+1
     if(indexPlusOne>=period) {
-      fmt.Println(index, period, values[indexPlusOne-period:indexPlusOne])
+      //fmt.Println(index, period, values[indexPlusOne-period:indexPlusOne])
       y := Mean(values[indexPlusOne-period:indexPlusOne])
       result[index] = y
     } else {
       result[index] = nil
     }
   }
+  return result
+}
+
+func Ema(values []interface{}, period int) []interface{}{
+  sma := Sma(values, period)
+  var result []interface{} = make([]interface{}, len(values))
+  var multiplier = (2.0 / (float64(period) + 1.0) )
+  for index,element := range values {
+    if(sma[index]==nil || result[index-1]==nil) {
+      result[index] = sma[index]
+    } else {
+      fmt.Println(element.(float64), result[index-1].(float64), (element.(float64) - result[index-1].(float64)))
+      result[index] = (element.(float64) - result[index-1].(float64)) * multiplier + result[index-1].(float64)
+    }
+  }
+  fmt.Println(period, multiplier, result)
   return result
 }
---
+++ ta/ma_test.go
@@ -0,0 +1,42 @@
+package ta
+
+import "testing"
+
+func TestSma(t *testing.T) {
+	cases := []struct {
+		in []float64
+    want float64
+	}{
+		{ []float64{ 0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0 }, 7.0 },
+    { []float64{ 0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0 }, 6.0 },
+	}
+	for _, c := range cases {
+    var interfaceSlice []interface{} = make([]interface{}, len(c.in))
+    for i, d := range c.in {
+      interfaceSlice[i] = d
+    }
+		got := Sma(interfaceSlice, 5)
+		if got[len(got)-1] != c.want {
+			t.Errorf("Sma(%q) == %q, want %q", c.in,got[len(got)-1], c.want)
+		}
+	}
+}
+
+func TestEma(t *testing.T) {
+	cases := []struct {
+		in []float64
+    want float64
+	}{
+		{ []float64{ 26.0, 54.0, 8.0, 77.0, 61.0, 39.0, 44.0, 91.0, 98.0, 17.0 }, 53.7917695473251 },
+	}
+	for _, c := range cases {
+    var interfaceSlice []interface{} = make([]interface{}, len(c.in))
+    for i, d := range c.in {
+      interfaceSlice[i] = d
+    }
+		got := Ema(interfaceSlice, 5)
+		if got[len(got)-1] != c.want {
+			t.Errorf("Ema(%q) == %q, want %q", c.in,got[len(got)-1], c.want)
+		}
+	}
+}
