4546bb3a004061c428297459ba89d5de29f48196
Luke Chadwick <me@vertis.io>
2015-08-29 14:46:07 +1000

    Working ATR and TrueRange. Kept commented version for history

---
+++ ta/atr.go
@@ -0,0 +1,60 @@
+package ta
+
+import (
+  "math"
+)
+
+type HighLowClose struct {
+  High float64
+  Low float64
+  Close float64
+}
+
+// Atr calculates the Average True Range of an array of HighLowClose structs given a period
+func Atr(values []HighLowClose, period int) []float64 {
+  // var atrs []float64
+  // hlc := values[0]
+  // tr := hlc.High-hlc.Low // since we have no previous entry, this is the True Range for the first period
+  // atr := tr * float64(1/period)
+  // atrs = append(atrs, atr)
+  // for idx,hlc := range values[1:] {
+  //   tr := max([]float64 { (hlc.High-hlc.Low), math.Abs(hlc.High - values[idx].Close), math.Abs(hlc.Low - values[idx].Close) })
+  //   atr = ((atrs[idx+period-1] * float64(period-1)) + tr) / float64(period)
+  //   atrs = append(atrs, atr)
+  // }
+  // return atrs
+  
+  var atrs []float64
+  startIdx := period
+  endIdx := len(values)-1
+  tr := TrueRange(values)
+  sma := Sma(tr[:startIdx], period)
+  fmt.Println(len(tr), len(sma))
+  atrs = append(atrs, sma[len(sma)-1])
+  for i := startIdx; i < endIdx; i++ {
+    lastAtr := atrs[len(atrs)-1]
+    atr := ((lastAtr*float64(period-1)) + tr[i]) / float64(period)
+    atrs = append(atrs, atr)
+  }
+  return atrs
+}
+
+func TrueRange(values []HighLowClose) []float64 {
+  var result []float64
+  for idx,hlc := range values[1:] {
+    tr := max([]float64 { (hlc.High-hlc.Low), math.Abs(hlc.High - values[idx].Close), math.Abs(hlc.Low - values[idx].Close) })
+    result = append(result, tr)
+  }
+  return result
+}
+
+func max(values []float64) float64 {
+  var curMax, prevMax float64
+  prevMax = values[0]
+  for idx,el := range values[1:] {
+    curMax = math.Max(values[idx], el)
+    curMax = math.Max(curMax, prevMax)
+    prevMax = curMax
+  }
+  return curMax
+}
---
+++ ta/atr_test.go
@@ -0,0 +1,37 @@
+package ta_test
+
+import (
+	. "github.com/vertis/gota/ta"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+var _ = Describe("Atr", func() {
+	It("should return the correct values", func() {
+		in := []HighLowClose {
+			HighLowClose{ High: 1.1, Low: 0.9, Close: 1.0 },
+			HighLowClose{ High: 2.1, Low: 1.9, Close: 2.0 },
+			HighLowClose{ High: 3.1, Low: 2.9, Close: 3.0 },
+			HighLowClose{ High: 4.1, Low: 3.9, Close: 4.0 },
+			HighLowClose{ High: 5.2, Low: 4.9, Close: 5.0 },
+		}
+		out := []float64{ 1.0999999999999999, 1.1333333333333333 }
+		result := Atr(in, 3)
+		Expect(result).To(Equal(out))
+	})
+})
+
+var _ = Describe("TrueRange", func() {
+	It("should return the correct values", func() {
+		in := []HighLowClose {
+			HighLowClose{ High: 1.1, Low: 0.9, Close: 1.0 },
+			HighLowClose{ High: 2.1, Low: 1.9, Close: 2.0 },
+			HighLowClose{ High: 3.1, Low: 2.9, Close: 3.0 },
+			HighLowClose{ High: 4.1, Low: 3.9, Close: 4.0 },
+			HighLowClose{ High: 5.2, Low: 4.9, Close: 5.0 },
+		}
+		out := []float64{ 1.1, 1.1, 1.0999999999999996, 1.2000000000000002 }
+		result := TrueRange(in)
+		Expect(result).To(Equal(out))
+	})
+})
