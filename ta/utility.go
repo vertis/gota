b997ad65130f8f20481aa0f9bb11968558dfdd60
Luke Chadwick <me@vertis.io>
2015-08-29 14:46:30 +1000

    Cleanup

--- ta/utility.go
+++ ta/utility.go
@@ -1,13 +1,11 @@
 package ta
 
 import (
-  "fmt"
   "math"
 )
 
 func minusArray(valuesA []float64, valuesB []float64) []float64 {
   sliceA, sliceB := normalizeSlice(valuesA, valuesB)
-  fmt.Println(len(sliceA), len(sliceB))
   var result []float64
   for index,element := range sliceA {
     result = append(result, element-sliceB[index])
