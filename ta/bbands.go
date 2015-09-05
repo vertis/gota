e392babf429434aa1f076c937a780bb4e2deda54
Luke Chadwick <me@vertis.io>
2015-09-05 11:35:09 +1000

    Commit failing test for bbands

---
+++ ta/bbands.go
@@ -0,0 +1,44 @@
+package ta
+
+import (
+  "math"
+)
+
+func BollingerBands(values []float64, period int) ([]float64, []float64, []float64) {
+  deviationsUp := 2.0
+  deviationsDown := 2.0
+
+  middleBand := Sma(values, period)
+  offset := len(values)-len(middleBand)
+  var upperBand []float64
+  var lowerBand []float64
+  for idx, v := range middleBand {
+    backIdx := offset+idx-period+1
+    if backIdx < 0 {
+      backIdx = 0
+    }
+    stdDev := SliceStdDev(values[backIdx:offset+idx+1])
+    upperBand = append(upperBand, v + (stdDev * deviationsUp))
+    lowerBand = append(lowerBand, v - (stdDev * deviationsDown))
+  }
+  return upperBand, middleBand, lowerBand
+}
+
+// SliceVariance returns the variance of the slice of float64.
+func SliceVariance(values []float64) float64 {
+	if 0 == len(values) {
+		return 0.0
+	}
+	m := Mean(values)
+	var sum float64
+	for _, v := range values {
+		d := v - m
+		sum += d * d
+	}
+	return sum / float64(len(values))
+}
+
+// SliceStdDev returns the standard deviation of the slice of float64.
+func SliceStdDev(values []float64) float64 {
+	return math.Sqrt(SliceVariance(values))
+}
