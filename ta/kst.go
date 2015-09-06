3a8f2be26ba5ba00cd4606a4066113e681ae5520
Luke Chadwick <me@vertis.io>
2015-09-06 18:51:54 +1000

    Add documentation. Cleanup

--- ta/kst.go
+++ ta/kst.go
@@ -5,6 +5,9 @@ import (
   //"fmt"
 )
 
+// Calculates Martin Prings' Know Sure Thing for a slice of float64
+// NB: I struggled to find an implementation to test against (it's not in TA-Lib). Therefore please test the accuracy of this
+// indicator before using
 func Kst(values []float64, rocPeriods []int, avgPeriods []int, signalPeriod int) ([]float64,[]float64) {
   var ksts []float64
   for i := 0; i < len(values); i++ {
@@ -21,39 +24,3 @@ func Kst(values []float64, rocPeriods []int, avgPeriods []int, signalPeriod int)
   kstSignals := Sma(ksts, signalPeriod)
   return ksts, kstSignals
 }
-
-// func Kst(values []float64, rocPeriods []int, avgPeriods []int, signalPeriod int) ([]float64,[]float64) {
-//   // TODO: Validate that I'm getting 4 roc periods and 4 avgPeriods
-//   maxRocPeriod := 0
-//   for _,el := range rocPeriods {
-//     maxRocPeriod = int(math.Max(float64(maxRocPeriod), float64(el)))
-//   }
-//   maxAvgPeriod := 0
-//   for _,el := range avgPeriods {
-//     maxAvgPeriod = int(math.Max(float64(maxAvgPeriod), float64(el)))
-//   }
-//   var ksts []float64
-//   var kstsignals []float64
-//   for idx,_ := range values[maxRocPeriod+maxAvgPeriod-1:] {
-//     valuesIdx := idx+maxRocPeriod+maxAvgPeriod+1
-//     fmt.Println(valuesIdx)
-//     roc1 := Roc(values[:valuesIdx],rocPeriods[0])
-//     roc2 := Roc(values[:valuesIdx],rocPeriods[1])
-//     roc3 := Roc(values[:valuesIdx],rocPeriods[2])
-//     roc4 := Roc(values[:valuesIdx],rocPeriods[3])
-//     //fmt.Println(roc1, roc2, roc3, roc4)
-//     sma1 := Sma(roc1,avgPeriods[0]) //Mean(roc1[len(roc1)-avgPeriods[0]:])
-//     sma2 := Sma(roc2,avgPeriods[1]) //Mean(roc2[len(roc2)-avgPeriods[1]:])
-//     sma3 := Sma(roc3,avgPeriods[2]) //Mean(roc3[len(roc3)-avgPeriods[2]:])
-//     sma4 := Sma(roc4,avgPeriods[3]) //Mean(roc4[len(roc4)-avgPeriods[3]:])
-//     //fmt.Println(sma1, sma2, sma3, sma4)
-//     kst := (sma1[len(sma1)-1] * 1) + (sma2[len(sma2)-1] * 2) + (sma3[len(sma3)-1] * 3) + (sma4[len(sma4)-1] * 4)
-//     ksts = append(ksts, kst)
-//     //fmt.Println(kst, len(ksts))
-//     if len(ksts) >= signalPeriod {
-//       signal := Mean(ksts[len(ksts)-signalPeriod:])
-//       kstsignals = append(kstsignals, signal)
-//     }
-//   }
-//   return ksts, kstsignals
-// }
